df08ad7a6f370534cd28c8172113c2d7fbf61640
NathaciaLais <nathacialais@gmail.com>
2023-06-07 14:00:23 +0700

    up main-login-register

--- app/src/main/java/Nathacia/uas/pendataanrs/MainActivity.java
+++ app/src/main/java/Nathacia/uas/pendataanrs/MainActivity.java
@@ -2,13 +2,23 @@ package Nathacia.uas.pendataanrs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import Nathacia.uas.pendataanrs.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        if (!Utilities.checkValue(MainActivity.this,"xUsername")){
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
